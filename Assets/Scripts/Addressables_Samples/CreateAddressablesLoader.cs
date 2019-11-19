5797230658d40d5193713a1486659d0b70b5272a
Badger <Badgerdox@gmail.com>
2019-11-18 20:23:08 -0700

    no message

--- Assets/Scripts/Addressables_Samples/CreateAddressablesLoader.cs
+++ Assets/Scripts/Addressables_Samples/CreateAddressablesLoader.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.ResourceLocations;
 
-public static class CreateAddressablesLoader 
+public static class CreateAddressablesLoader
 {
-    public static async Task ByLoadedAddress<T>(IList<IResourceLocation> loadedLocations, List<T> createdObjs)
+    public static async Task InitAssets<T>(string assetNameOrLabel, List<T> createdAssets)
         where T : Object
+    {
+        var locations = await Addressables.LoadResourceLocationsAsync(assetNameOrLabel).Task;
+            foreach (var location in locations)
+                createdAssets.Add(await Addressables.InstantiateAsync(location).Task as T);
+    }
+    
+    public static async Task ByLoadedAddress<T>(IList<IResourceLocation> loadedLocations, List<T> createdObjs)
+    where T : Object
     {
         foreach (var location in loadedLocations)
         {
@@ -16,162 +26,52 @@ public static class CreateAddressablesLoader
             createdObjs.Add(obj);
         }
     }
+
+    public static async Task HasAllLabels<T>(List<string> labels, bool removeDupes, List<T> createdObjs) where T : Object
+    {
+        IList<IResourceLocation> locations = new List<IResourceLocation>();
+
+        foreach (var label in labels)
+        {
+            foreach (var resourceLocation in await Addressables.LoadResourceLocationsAsync(label).Task) 
+                locations.Add(resourceLocation);
+        }
+
+        if (removeDupes)
+            await FindDuplicateEntries(locations, true);
+        
+        foreach (var location in locations)
+        {
+            createdObjs.Add(await Addressables.InstantiateAsync(location).Task as T);
+        }
+    }
+    
+    public static async Task BySpecificLabels<T>(List<string> labels, List<T> createdObjects) where T : Object
+    {
+        IList<IResourceLocation> locations = new List<IResourceLocation>();
+
+        foreach (var label in labels)
+        {
+            var tempLoc = await Addressables.LoadResourceLocationsAsync(label).Task;
+            locations = tempLoc.ToList();
+        }
+        
+        foreach (var location in locations)
+            createdObjects.Add(await Addressables.InstantiateAsync(location).Task as T);
+    }
+
+    private static async Task FindDuplicateEntries(IList<IResourceLocation> locations, bool toRemove = false, bool toGet = false)
+    {
+       var dups = locations.GroupBy(s => s)
+            .Where(g => g.Count() > 1)
+            .Select(y => y.Key)
+            .ToList();
+
+        if (toRemove)
+            foreach (var dup in dups)
+                locations.Remove(dup);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//public static async Task ByAddress<T>(string label, List<T> createdAssets) where T : Object
-//    {
-//       var tempLocations = new List<IResourceLocation>();
-//       await AddressableLocationLoader.GetAll(label, tempLocations);
-//        
-//        foreach (var location in tempLocations)
-//        {
-//            var obj = await Addressables.InstantiateAsync(location).Task as T;
-//            
-//            createdAssets.Add(obj);
-//        }
-//    }
-//
-//    public static async Task ByLabel<T>(string label, List<T> createdAssets) where T : Object
-//    {
-//        var locations = await Addressables.LoadResourceLocationsAsync(label).Task;
-//
-//        foreach (var location in locations)
-//        {
-//            createdAssets.Add(await Addressables.InstantiateAsync(location).Task as T);
-//        }
-//    }
-//
-//    public static async Task ByName<T>(string label, string assetName, List<T> createdObjects = null) where T : Object
-//    {
-//        var locations = await Addressables.LoadResourceLocationsAsync(label).Task;
-//
-//        foreach (var location in locations)
-//        {
-//            if (!location.PrimaryKey.Contains(assetName)) break;
-//
-//            if (createdObjects == null)
-//            {
-//                Addressables.InstantiateAsync(assetName); 
-//            }
-//            else
-//            {
-//                createdObjects.Add(await Addressables.InstantiateAsync(assetName).Task as T);
-//            }
-//        }
-//    }
-//
-//    public static async Task SearchByLabels<T>(List<string> labels, bool removeDupes, List<T> createdObjs) where T : Object
-//    {
-//        IList<IResourceLocation> locations = new List<IResourceLocation>();
-//
-//        foreach (var label in labels)
-//        {
-//            foreach (var resourceLocation in await Addressables.LoadResourceLocationsAsync(label).Task) 
-//                locations.Add(resourceLocation);
-//        }
-//
-//        if (removeDupes)
-//            await FindDuplicateEntries(locations, true);
-//
-//        if (createdObjs == null)
-//        {
-//            foreach (var location in locations)
-//                await Addressables.InstantiateAsync(location).Task;
-//        }
-//        else
-//        {
-//            foreach (var location in locations)
-//            {
-//                createdObjs.Add(await Addressables.InstantiateAsync(location).Task as T);
-//            }
-//        }
-//    }
-//
-//    public static async Task ContainingAllLabels<T>(List<string> labels, List<T> createdObjects = null) where T : Object
-//    {
-//        IList<IResourceLocation> locations = new List<IResourceLocation>();
-//
-//        foreach (var label in labels)
-//        {
-//            foreach (var resourceLocation in await Addressables.LoadResourceLocationsAsync(label).Task)
-//                locations.Add(resourceLocation);
-//        }
-//
-//        await FindDuplicateEntries(locations, false, true);
-//
-//        if (createdObjects == null)
-//        {
-//            foreach (var location in locations)
-//                await Addressables.InstantiateAsync(location).Task;
-//        }
-//        else
-//        {
-//            foreach (var location in locations)
-//            {
-//                createdObjects.Add(await Addressables.InstantiateAsync(location).Task as T);
-//            }
-//        }
-//    }
-//
-//    private static async Task FindDuplicateEntries(IList<IResourceLocation> locations, bool toRemove = false, bool toGet = false)
-//    {
-//       var dups = locations.GroupBy(s => s)
-//            .Where(g => g.Count() > 1)
-//            .Select(y => y.Key)
-//            .ToList();
-//
-//        if (toRemove)
-//            foreach (var dup in dups)
-//                locations.Remove(dup);
-//
-//        if (toGet)
-//        {
-//            foreach (var dup in dups)
-//                for (int i = locations.Count - 1; i >= 0; i--)
-//                    if (!dup.PrimaryKey.Equals(locations[i].PrimaryKey))
-//                        locations.RemoveAt(i);
-//        }
-//    }
-//}
-
-
-
-
 
 
 
--- Assets/Scripts/Addressables_Samples/CreatedAssets.cs
+++ Assets/Scripts/Addressables_Samples/CreatedAssets.cs
@@ -6,88 +6,27 @@ using UnityEngine.AddressableAssets;
 
 public class CreatedAssets : MonoBehaviour
 {
-  private LoadedAddressableLocations _loadedLocations;
-  
-  [SerializeField] private List<GameObject> Assets { get; } = new List<GameObject>();
+    [SerializeField] private string _assetName;
+    [SerializeField] private string _label;
+    [SerializeField] private List<string> _labels = new List<string>();
 
-  private void Start()
-  {
-    CreateAndWaitUntilCompleted();
-  }
+    [SerializeField] private List<GameObject> Assets { get; } = new List<GameObject>();
 
-  private async Task CreateAndWaitUntilCompleted()
-  {
-    _loadedLocations = GetComponent<LoadedAddressableLocations>();
-
-    await Task.Delay(TimeSpan.FromSeconds(1));
-
-    await CreateAddressablesLoader.ByLoadedAddress(_loadedLocations.AssetLocations, Assets);
-
-    foreach (var asset in Assets)
+    private void Start()
     {
-      Debug.Log(asset.name);
+        CreateAndWaitUntilCompleted();
     }
-  }
-  
-  
-  
+
+    private async Task CreateAndWaitUntilCompleted()
+    {
+        //await CreateAddressablesLoader.InitAssets(_assetName, Assets);
+        //await CreateAddressablesLoader.HasAllLabels(_labels, true, Assets)
+        await CreateAddressablesLoader.BySpecificLabels(_labels, Assets);
+
+        foreach (var asset in Assets)
+        {
+            //Asset is now loaded perform additional operations
+            Debug.Log(asset.name);
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    //await CreateAddressablesLoader.ByAddress(_label, Assets);
-//        //await CreateAddressablesLoader.ByName(_label, "Circle", Assets);
-//        //await CreateAddressablesLoader.ByLabel(_label, Assets);
-//        //await CreateAddressablesLoader.SearchByLabels(_labels, true, Assets);
-//        await CreateAddressablesLoader.ContainingAllLabels(_labels, Assets);
-//        
-//        //OBJECT(S) NOW FULLY LOADED
-//        AfterLoadActions();
-//    }
-//
-//    private void AfterLoadActions()
-//    {
-//        foreach (var asset in Assets)
-//        {
-//            //asset.transform.SetParent(GetComponent<Transform>());
-//            //asset.SetActive(false);
-//        }
-//    }
-//
-//    private void ClearAsset(GameObject go)
-//    {
-//        Addressables.Release(go);
-//    }
-//
-//    private void ClearAssets(List<GameObject> gos)
-//    {
-//        foreach (var go in gos)
-//        {
-//            Addressables.Release(go);
-//        }
-//    }
-//}
