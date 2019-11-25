6f000c44f15da7fa926bdaa3da1646ee4da1c5ef
Badger <Badgerdox@gmail.com>
2019-11-24 19:35:50 -0700

    no message

---
+++ Assets/Scripts/Addressables_Samples/SortedCreatedAssets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class SortedCreatedAssets : MonoBehaviour
+{
+    [SerializeField] private List<string> _labels = new List<string>();
+
+    private void Start()
+    {
+        SortWaitToComplete(_labels);
+    }
+
+    private async Task SortWaitToComplete(List<string> labels)
+    {
+        var locations = await Addressables.LoadResourceLocationsAsync
+            (labels.ToArray(), Addressables.MergeMode.Intersection).Task;
+
+        foreach (var location in locations)
+        {
+            Addressables.InstantiateAsync(location);
+        }
+    }
+}
