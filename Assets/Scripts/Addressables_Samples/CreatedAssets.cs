ec2dc63543f4501f85ce16744c9c30f7137368d4
Badgerdox <50283631+Badgerdox@users.noreply.github.com>
2020-02-08 15:46:16 -0700

    Update CreatedAssets.cs

--- Assets/Scripts/Addressables_Samples/CreatedAssets.cs
+++ Assets/Scripts/Addressables_Samples/CreatedAssets.cs
@@ -10,8 +10,6 @@ public class CreatedAssets : MonoBehaviour
 {
     [SerializeField] private string _label;
     private List<GameObject> Assets { get; } = new List<GameObject>();
-    
-    
 
     private void Start()
     {
@@ -39,62 +37,3 @@ public class CreatedAssets : MonoBehaviour
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//[SerializeField] private string _assetName;
-//[SerializeField] private string _label;
-//[SerializeField] private List<string> _labels = new List<string>();
-//
-//[SerializeField] private List<GameObject> Assets { get; } = new List<GameObject>();
-//
-//private void Start()
-//{
-//CreateAndWaitUntilCompleted();
-//}
-//
-//private async Task CreateAndWaitUntilCompleted()
-//{
-////await CreateAddressablesLoader.InitAssets(_assetName, Assets);
-////await CreateAddressablesLoader.HasAllLabels(_labels, true, Assets);
-//await CreateAddressablesLoader.BySpecificLabels(_labels, Assets);
-//
-//    foreach (var asset in Assets)
-//{
-//    //Asset is now loaded perform additional operations
-//    Debug.Log(asset.name);
-//}
-//}
-
-
